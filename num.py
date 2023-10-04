3eb419bfa6e49e67615b1beca98de7b8a0b40166
Bernat Dosrius <bernat.dosrius@gmail.com>
2023-10-04 18:00:21 +0200

    Actualitzacio conjunta amb la colaboració de tot el grup
    Hem arreglat molts errors de codi
    Ja funciona el cas 2x2
    En el cas 3x3 no congrueix

--- num.py
+++ num.py
@@ -1,10 +1,11 @@
 import numpy as np
+import time
 from funcions_matrius import matriu_random, matriu_guanyadora, trobar_zero
 import math
 import random
 import matplotlib.pyplot as plt
 
-N = 1
+N = 2
 M = 2
 
 def __codificacio__(taulell):
@@ -18,13 +19,14 @@ def __codificacio__(taulell):
     return index
 
 class ESTRATEGIA_EXPL():
-    def __init__(self, inici, final, caiguda):
+    def __init__(self, inici, final, caiguda):  # inici=1, final=0.05, caiguda=epsilon
         self.inici = inici
         self.final = final
         self.caiguda = caiguda
 
     def act_rati_expl(self, pas_actual):
         return self.final + (self.inici - self.final)  * math.exp(-1 * pas_actual * self.caiguda)
+    #   0.05 + (0.95)/e^p/10 ben def
 
 class AGENT:
     def __init__(self, estrategia, n_accions):
@@ -33,16 +35,11 @@ class AGENT:
         self.n_accions = n_accions
     
     def seleccionar_accio(self, estat, q_table):
-        rati = self.estrategia.act_rati_expl(self.pas_actual)
-        self.pas_actual += 1
+        accio = np.argmax(q_table[__codificacio__(estat)])
+        if random.random() < self.estrategia.caiguda:
+            accio = random.randint(0, self.n_accions - 1)
 
-        if rati > random.random():
-            return random.randrange(self.n_accions)
-        else:
-            return q_table[__codificacio__(estat)].argmax()
-
-
-    
+        return accio   
 
 
 class GAME:
@@ -57,7 +54,7 @@ class GAME:
         
         self.n_accions = 4
         
-        self.accions = [[-1,0],[1,0],[0,-1],[0,1]]
+        self.accions = [[-1,0],[1,0],[0,1],[0,-1]]
         
         self.n_estats = math.factorial(N*M)
         
@@ -72,6 +69,7 @@ class GAME:
     
     def reset(self):
         self.taulell = self.taulell_ini.copy()
+        self.pos_0 = trobar_zero(self.taulell)
         return
 
     """
@@ -81,7 +79,7 @@ class GAME:
     (0,-1) -> 0 cap a esquerra    
     """ 
 
-    def is_valid_move(self, move):
+    def is_valid_move(self, move):  # move[baix, dreta]
         if (self.pos_0[0] + move[0] < 0 or self.pos_0[1]+move[1] < 0): return False
         if (self.pos_0[0] + move[0] > N-1 or self.pos_0[1]+move[1] > M-1): return False
         return True
@@ -93,20 +91,28 @@ class GAME:
         return self.taulell, -1
 
     def actualitzar_q_table(self, move, alpha, gamma, agent):
+        vell_estat = self.taulell.copy()
         if not self.is_valid_move(self.accions[move]):
             nou_estat = self.taulell
-            recompensa = -10
-        else: 
-            nou_estat, recompensa = self.prendre_nou_estat(self.accions[move])
+            recompensa = -100
+        else:
+            nou_estat, recompensa = self.prendre_nou_estat(self.accions[move])  # canvia taulell
         nova_accio = agent.seleccionar_accio(nou_estat, self.q_table)
-        self.q_table[__codificacio__(self.taulell), move] = alpha*(recompensa + gamma*self.q_table[__codificacio__(nou_estat), nova_accio] - self.q_table[__codificacio__(self.taulell), move])
+        self.q_table[__codificacio__(vell_estat), move] = alpha*(recompensa + gamma*(self.q_table[__codificacio__(nou_estat), nova_accio]) - self.q_table[__codificacio__(vell_estat), move])
+
+
+
+def juga_pas(game):
+    index = np.argmax(game.q_table[__codificacio__(game.taulell)])
+    accio = game.accions[index]
+    if (not game.is_valid_move(accio)):
+        raise Exception("puta ia")
+    
+    game.prendre_nou_estat(accio)
+    
 
 def main1():
     game = GAME()
-    
-    print("Taulell:")
-    print(game.taulell)
-
     while(not game.guanyat()):
         user = input("introdueix moviment(wasd): ")
 
@@ -128,11 +134,11 @@ def main1():
 
 def main2():
 
-    n_episodes = 10000
-    max_iter = 20
+    n_episodes = 100
+    max_iter = 50
 
     alpha = 0.5
-    gamma = 1
+    gamma = 1   
     epsilon = 0.1
 
     estrategia = ESTRATEGIA_EXPL(1, 0.05, epsilon)
@@ -142,15 +148,29 @@ def main2():
 
     for i in range(n_episodes):
         iter = 0
-        game.reset()
         while(not game.guanyat() and iter < max_iter):
             accio = agent.seleccionar_accio(game.taulell, game.q_table)
             game.actualitzar_q_table(accio, alpha, gamma, agent)
             iter += 1
-        print(f"Episode: {i}, Moviments: {iter}")
+        game.reset()
+        print(f"Episode: {i}, Moviments: {iter}") #Taulell: \n {game.taulell}
         pasos.append(iter)
     
     plt.plot(pasos)
     print(f"Min pasos: {min(pasos)}")
+
+    print(game.q_table)
+    game.reset()
     
-main2()
+    print("Taulell:")
+    print(game.taulell)
+    while (not game.guanyat()):
+        juga_pas(game)
+        print("Taulell:")
+        print(game.taulell)
+        time.sleep(1)
+    print("Visca la ia")
+    
+    
+if __name__ == "__main__":
+    main2()
